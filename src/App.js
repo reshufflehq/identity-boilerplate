d4c79bb7f001b3b7db6f6956f6cee3dd8dea2f38
Ryland Goldstein <ryland@binaris.com>
2019-11-19 19:28:15 +0000

    Very minimal boilerplate showcasing auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,76 @@
+import '@reshuffle/code-transform/macro';
 import React from 'react';
 
+import { useAuth } from '@reshuffle/react-auth';
+
+const centerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+const wrapperStyle = {
+  ...centerStyle,
+  width: '100%',
+  height: '100%',
+};
+
+const contentStyle = {
+  ...centerStyle,
+  width: '40%',
+  height: '20%',
+};
+
+const profileStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+}
+
+function ContentWrapper(props) {
+  return (
+    <div style={wrapperStyle}>
+      <div style={contentStyle}>
+        {props.children}
+      </div>
+    </div>
+  );
+}
+
 function App() {
-  return (<div></div>);
+  const {
+    loading,
+    error,
+    authenticated,
+    profile,
+    getLoginURL,
+    getLogoutURL,
+  } = useAuth();
+
+  if (loading) {
+    return <ContentWrapper><h2>Loading...</h2></ContentWrapper>;
+  }
+  if (error) {
+    return <ContentWrapper><h1>{error.toString()}</h1></ContentWrapper>;
+  }
+
+  return (
+    <ContentWrapper>
+      {
+        authenticated ? (
+          <div style={profileStyle}>
+            <img src={profile.picture}
+                 height={20}
+                 alt='user profile'
+            />
+            <span>{profile.displayName}</span>
+            <a href={getLogoutURL()}>Logout</a>
+          </div>
+        ) : (
+          <a href={getLoginURL()}>Login</a>
+        )
+      }
+    </ContentWrapper>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import { AuthProvider } from '@reshuffle/react-auth';
 
+import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
